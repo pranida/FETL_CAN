b36e1c41c662b510d4a301c473f6e8c7b8116945
canUNG <pranidaya1@gmail.com>
2017-05-18 14:50:37 +0700

    1805171450OK

--- app/src/main/java/fetl/praniday/fetl_can/DetailActivity.java
+++ app/src/main/java/fetl/praniday/fetl_can/DetailActivity.java
@@ -2,12 +2,51 @@ package fetl.praniday.fetl_can;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private ImageView backImageView,imageView;
+    private TextView titleTextview, detailTextview;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //Intial view
+        intialView();
+
+        //show view
+        showView();
+
+
     }
-}
+
+    private void showView() {
+
+        String strTitle = getIntent().getStringExtra("Name" );
+        String strDetail = getIntent().getStringExtra("Detail");
+        String strIcon = getIntent().getStringExtra("Icon");
+
+        titleTextview.setText(strTitle);
+        detailTextview.setText(strDetail);
+        Picasso.with(DetailActivity.this).load(strIcon).into(imageView);
+
+    }
+
+    private void intialView() {
+        backImageView = (ImageView) findViewById(R.id.imvBack);
+        imageView = (ImageView) findViewById(R.id.imvImage);
+        titleTextview = (TextView) findViewById(R.id.txtTitle);
+        detailTextview = (TextView) findViewById(R.id.txtDetail);
+    }
+
+
+
+}//main cladd
--- app/src/main/java/fetl/praniday/fetl_can/ServiceActivity.java
+++ app/src/main/java/fetl/praniday/fetl_can/ServiceActivity.java
@@ -1,11 +1,17 @@
 package fetl.praniday.fetl_can;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ServiceActivity extends AppCompatActivity {
 //Explicit
@@ -47,6 +53,41 @@ public class ServiceActivity extends AppCompatActivity {
             String strJSON = getData.get();
             Log.d("18MayV1" , "JSON ==> " + strJSON);
 
+
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+            int i = jsonArray.length();
+            final String[] iconString = new String[i];
+            final String[] titleString = new String[i];
+            final String[] detailString = new String[i];
+
+
+            for (int i1=0;i1<i;i1++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i1);
+                iconString[i1] = jsonObject.getString("Image");
+                titleString[i1] = jsonObject.getString("Produce");
+                detailString[i1] = jsonObject.getString("Detail");
+
+            }//for
+
+            MyAdapter myAdapter = new MyAdapter(this,iconString,titleString,detailString);
+            listView.setAdapter(myAdapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    Intent intent = new Intent(ServiceActivity.this,DetailActivity.class);
+                    intent.putExtra("Name", titleString[position]);
+                    intent.putExtra("Detail", detailString[position]);
+                    intent.putExtra("Icon", iconString[position]);
+                    startActivity(intent);
+                }
+            });
+
+
+
         } catch (Exception e) {
 
             Log.d("18MayV1" , "e createListview ==> " + e.toString());
@@ -55,7 +96,7 @@ public class ServiceActivity extends AppCompatActivity {
 
     private void showText() {
         nameString = getIntent().getStringExtra("Login");
-        textView.setText("Login by : " + nameString);
+        textView.setText("Login : " + nameString);
     }
 
     private void initialView() {
