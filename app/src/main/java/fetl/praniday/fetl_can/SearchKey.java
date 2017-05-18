0613421d05f87e42edb387262eb22d30bb238ae8
canUNG <pranidaya1@gmail.com>
2017-05-18 16:20:44 +0700

    1805171620OK

---
+++ app/src/main/java/fetl/praniday/fetl_can/SearchKey.java
@@ -0,0 +1,51 @@
+package fetl.praniday.fetl_can;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+/**
+ * Created by Pranida.Y on 05/18/17.
+ */
+
+public class SearchKey extends AsyncTask<String, Void,String>{
+
+    private Context context;
+
+    public SearchKey(Context context) {
+        this.context = context;
+    }
+
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        try {
+
+            MyConstant myConstant = new MyConstant();
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody requestBody = new FormEncodingBuilder()
+                    .add("isAdd" ,"true")
+                    .add("Keycode" , params[0])
+                    .build();
+            Request.Builder builder = new Request.Builder();
+            Request request = builder .url(myConstant.getUrlGetProduceWhereCode())
+                    .post(requestBody).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return  response.body().string();
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+}//main class
--- app/src/main/java/fetl/praniday/fetl_can/ServiceActivity.java
+++ app/src/main/java/fetl/praniday/fetl_can/ServiceActivity.java
@@ -13,12 +13,12 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class ServiceActivity extends AppCompatActivity {
+public class ServiceActivity extends AppCompatActivity implements View.OnClickListener {
 //Explicit
     private TextView textView;
     private ImageView barImageView ,qrImageView;
     private ListView listView ;
-    private  String nameString;
+    private  String nameString , myCodeString;
 
 
 
@@ -38,8 +38,48 @@ public class ServiceActivity extends AppCompatActivity {
         //Create listview
         createListView();
 
+        //controller
+        controller();
+
+
 
     }//main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            myCodeString = data.getStringExtra("SCAN_RESULT");
+            Log.d("18MayV2" , "mycodestring ==> " + myCodeString);
+
+            try {
+
+                SearchKey searchKey = new SearchKey(this);
+                searchKey.execute(myCodeString);
+                String strJSON = searchKey.get();
+
+                JSONArray jsonArray = new JSONArray( strJSON);
+                JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+                Intent intent = new Intent(ServiceActivity.this, DetailActivity .class);
+                intent.putExtra("Name", jsonObject.getString("Produce"));
+                intent.putExtra("Detail", jsonObject.getString("Detail"));
+                intent.putExtra("Icon", jsonObject.getString("Image"));
+                startActivity(intent);
+
+
+            } catch (Exception e) {
+                Log.d("18MayV2" , " e onac ==> " + e.toString());
+
+            }
+        }
+
+    }
+
+    private void controller() {
+        barImageView.setOnClickListener(this);
+        qrImageView.setOnClickListener(this);
+    }
 
     private void createListView() {
 
@@ -107,5 +147,29 @@ public class ServiceActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
 
+        int i = 0;
+        String [] codeStrings = new  String[] {"BAR_CODE_MODE","QR_CODE_MODE"};
+
+        if (v == barImageView) {
+            i = 0;
+        }
+        if (v == qrImageView) {
+            i = 1;
+        }
+
+
+        try {
+
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent .putExtra("SCAN_MODE" , codeStrings[i]);
+            startActivityForResult(intent , 0);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }//main class
